55b27aef67d6fe406e657264d2236f4cd3aebbfd
Carmine T. Guida <carmine@carmine.com>
2019-08-16 22:02:54 -0400

    Lecture 5

--- Libraries/shaders/fragment_textured.glsl
+++ Libraries/shaders/fragment_textured.glsl
@@ -1,19 +1,7 @@
 
-uniform sampler2D texture;
-uniform float saturationAmount;
+uniform sampler2D diffuse;
 varying vec2 texCoordVar;
 
-vec3 saturation_func(vec3 rgb, float adjustment)
-{
-    const vec3 W = vec3(0.2125, 0.7154, 0.0721);
-    vec3 intensity = vec3(dot(rgb, W));
-    return mix(intensity, rgb, adjustment);
-}
-
-void main()
-{
-    vec4 finalColor;
-    finalColor.xyz = saturation_func(texture2D( texture, texCoordVar).xyz, saturationAmount);
-    finalColor.a = texture2D( texture, texCoordVar).a;
-    gl_FragColor = finalColor;
+void main() {
+    gl_FragColor = texture2D(diffuse, texCoordVar);
 }
