d28570b2db2e63246d4470596d5b1954a2f1b396
Carmine T. Guida <35237506+carmineguida@users.noreply.github.com>
2019-09-25 11:40:16 -0400

    Windows fix

--- Examples/Entity/Entity.h
+++ Examples/Entity/Entity.h
@@ -1,6 +1,10 @@
 #pragma once
 #define GL_SILENCE_DEPRECATION
 
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
@@ -23,5 +27,6 @@ public:
     void Update(float deltaTime);
     void Render(ShaderProgram *program);
 };
+
 
 
