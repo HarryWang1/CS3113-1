499e283fee2a7239af7d6dea2a7c9cd611cd8181
kp1732 <47366772+kp1732@users.noreply.github.com>
2019-11-17 18:20:06 -0500

    Added multiple level functionality

--- Projects/P5/SDLProject/main.cpp
+++ Projects/P5/SDLProject/main.cpp
@@ -29,6 +29,7 @@ bool gameIsRunning = true;
 bool gameWon = false;
 bool gameLost = false;
 glm::vec3 startPosition = glm::vec3(-4, 3, 0);
+int currentLevel = 0; // level count starts from zero for reasons ;^)
 
 
 // define level count
@@ -47,6 +48,10 @@ glm::vec3 startPosition = glm::vec3(-4, 3, 0);
 // define live enemy count for game and helper flag
 bool start = true;
 int liveCount = 0;
+
+
+// define enemy count for each level in the game
+int enemyCount[LEVELS] = {1};
 
 
 //define GameState object - will keep track of objects in the game
@@ -90,7 +95,7 @@ GLuint LoadTexture(const char* filePath) {
         std::cout << "Unable to load image. Make sure the path is correct\n";
         assert(false);
     }
-
+    
     GLuint textureID;
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
@@ -102,6 +107,7 @@ GLuint LoadTexture(const char* filePath) {
     stbi_image_free(image);
     return textureID;
 }
+
 
 
 // define player initialization function
@@ -125,10 +131,11 @@ void initPlayer(Entity* player) {
 }
 
 
+
 // define enemy initialization function
-void initEnemy(Entity* enemies, GLuint textures[]) {
+void initEnemy(Entity* enemies, GLuint* textures, int enemy_count) {
     
-    for (int i = 0; i < MAX_ENEMY; i++) {
+    for (int i = 0; i < enemy_count; i++) {
         // initialize enemy attributes
         enemies[i].entityType = ENEMY;
         enemies[i].isStatic = false;
@@ -143,6 +150,47 @@ void initEnemy(Entity* enemies, GLuint textures[]) {
         std::memcpy(states[0].enemies[i].texCoords, enemy_texCoords, sizeof(states[0].enemies[i].texCoords));
     }
 }
+
+
+
+// define ground platform initialization function
+void initgPlatform(Entity* platforms, GLuint texture) {
+    
+    // loop through and initialize ground platform
+    float platform_vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float platform_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    for (int i = 0; i < 10; i++) {
+        states[0].platforms[i].entityType = PLATFORM;
+        states[0].platforms[i].textureID = texture;
+        states[0].platforms[i].position = glm::vec3(i - 4.5f, -3.5f, 0);
+        std::memcpy(states[0].platforms[i].vertices, platform_vertices, sizeof(states[0].platforms[i].vertices));
+        std::memcpy(states[0].platforms[i].texCoords, platform_texCoords, sizeof(states[0].platforms[i].texCoords));
+    }
+}
+
+
+
+// define banner initialization function
+void initBanner(Entity* banners, GLuint* textures) {
+    
+    // initialize win banner attributes
+    banners[0].isStatic = true;
+    banners[0].textureID = textures[0];
+    float banner1_vertices[] = { -2.5, -0.25, 2.5, -0.25, 2.5, 0.25, -2.5, -0.25, 2.5, 0.25, -2.5, 0.25 };
+    float banner1_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    std::memcpy(banners[0].vertices, banner1_vertices, sizeof(banners[0].vertices));
+    std::memcpy(banners[0].texCoords, banner1_texCoords, sizeof(banners[0].texCoords));
+    
+    // initialize lose banner attributes
+    banners[1].isStatic = true;
+    banners[1].textureID = textures[0];
+    float banner2_vertices[] = { -2.5, -0.25, 2.5, -0.25, 2.5, 0.25, -2.5, -0.25, 2.5, 0.25, -2.5, 0.25 };
+    float banner2_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    std::memcpy(banners[1].vertices, banner2_vertices, sizeof(banners[1].vertices));
+    std::memcpy(banners[1].texCoords, banner2_texCoords, sizeof(banners[1].texCoords));
+}
+
+
 
 // define initialize function
 void Initialize() {
@@ -164,10 +212,12 @@ void Initialize() {
     glewInit();
 #endif
 
+    // init viewport
     glViewport(0, 0, 640, 480);
 
     // load texture shader
     program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
     
     // initialize player in all levels
     for (int i = 0; i < LEVELS; i++) {
@@ -176,107 +226,43 @@ void Initialize() {
     }
     
 
-    // initialize enemy attributes in all levels
+    // initialize enemy entities in all levels
     GLuint enemyLeft = LoadTexture("enemy_left.png");
     GLuint enemyRight = LoadTexture("enemy_right.png");
     for (int i = 0; i < LEVELS; i++) {
-        GLuint textures[2] = {enemyLeft, enemyRight};
-        initEnemy(states[i].enemies, textures);
+        GLuint enemy_textures[2] = {enemyLeft, enemyRight};
+        initEnemy(states[i].enemies, enemy_textures, enemyCount[i]);
     }
     
-    // enemy positions
+    // initlize some other enemy ettributes
+    // level 1
     states[0].enemies[0].position = glm::vec3(4, 4, 0);
-    states[0].enemies[1].position = glm::vec3(0, 3, 0);
-    states[0].enemies[2].position = glm::vec3(0, 3, 0);
-    // enemy states[0]
     states[0].enemies[0].entityState = STILL;
-    states[0].enemies[1].entityState = WALKING;
-    states[0].enemies[2].entityState = AI;
-    // enemy default direction
     states[0].enemies[0].entityDir = LEFT;
-    states[0].enemies[1].entityDir = LEFT;
-    states[0].enemies[2].entityDir = AUTO;
-    // enemy default texture
     states[0].enemies[0].textureID = states[0].enemies[0].textures[0];
-    states[0].enemies[1].textureID = states[0].enemies[1].textures[0];
-    states[0].enemies[2].textureID = states[0].enemies[2].textures[1];
+    
+    // level 2
+    // level 3
 
-
+    
     //load platform textures
     GLuint groundTextureID = LoadTexture("ground_stone.png");
-    GLuint grassTextureID = LoadTexture("air_stone.png");
-
-
-    // loop through and initialize ground platform
-    float platform_vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
-    float platform_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-
-    for (int i = 0; i < 10; i++) {
-        states[0].platforms[i].entityType = PLATFORM;
-        states[0].platforms[i].textureID = groundTextureID;
-        states[0].platforms[i].position = glm::vec3(i - 4.5f, -3.5f, 0);
-        std::memcpy(states[0].platforms[i].vertices, platform_vertices, sizeof(states[0].platforms[i].vertices));
-        std::memcpy(states[0].platforms[i].texCoords, platform_texCoords, sizeof(states[0].platforms[i].texCoords));
+    GLuint airTextureID = LoadTexture("air_stone.png");
+    // initialize ground platform in all levels
+    for (int i = 0; i < LEVELS; i++) {
+        GLuint plat_textures[2] = {groundTextureID, airTextureID};
+        initgPlatform(states[i].platforms, plat_textures[0]);
     }
-
-
-    // initialize platforms in the air
-    states[0].platforms[10].entityType = PLATFORM;
-    states[0].platforms[10].textureID = grassTextureID;
-    states[0].platforms[10].position = glm::vec3(-4.5f, 1.50f, 0);
-    std::memcpy(states[0].platforms[10].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[10].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-    states[0].platforms[11].entityType = PLATFORM;
-    states[0].platforms[11].textureID = grassTextureID;
-    states[0].platforms[11].position = glm::vec3(-3.5f, 1.50f, 0);
-    std::memcpy(states[0].platforms[11].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[11].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-    states[0].platforms[12].entityType = PLATFORM;
-    states[0].platforms[12].textureID = grassTextureID;
-    states[0].platforms[12].position = glm::vec3(-2.5f, 1.50f, 0);
-    std::memcpy(states[0].platforms[12].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[12].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-
-    states[0].platforms[13].entityType = PLATFORM;
-    states[0].platforms[13].textureID = grassTextureID;
-    states[0].platforms[13].position = glm::vec3(2.5f, -0.50f, 0);
-    std::memcpy(states[0].platforms[13].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[13].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-    states[0].platforms[14].entityType = PLATFORM;
-    states[0].platforms[14].textureID = grassTextureID;
-    states[0].platforms[14].position = glm::vec3(3.5f, -0.50f, 0);
-    std::memcpy(states[0].platforms[14].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[14].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-    states[0].platforms[15].entityType = PLATFORM;
-    states[0].platforms[15].textureID = grassTextureID;
-    states[0].platforms[15].position = glm::vec3(4.5f, -0.50f, 0);
-    std::memcpy(states[0].platforms[15].vertices, platform_vertices, sizeof(states[0].platforms[0].vertices));
-    std::memcpy(states[0].platforms[15].texCoords, platform_texCoords, sizeof(states[0].platforms[0].texCoords));
-
-
-    // initialize win banner attributes
-    states[0].banners[0].isStatic = true;
-    // initialize win banner textures
-    states[0].banners[0].textureID = LoadTexture("win.png");
-    float banner1_vertices[] = { -2.5, -0.25, 2.5, -0.25, 2.5, 0.25, -2.5, -0.25, 2.5, 0.25, -2.5, 0.25 };
-    float banner1_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-    std::memcpy(states[0].banners[0].vertices, banner1_vertices, sizeof(states[0].banners[0].vertices));
-    std::memcpy(states[0].banners[0].texCoords, banner1_texCoords, sizeof(states[0].banners[0].texCoords));
-
-
-    // initialize lose banner attributes
-    states[0].banners[1].isStatic = true;
-    // initialize lose banner textures
-    states[0].banners[1].textureID = LoadTexture("lost.png");
-    float banner2_vertices[] = { -2.5, -0.25, 2.5, -0.25, 2.5, 0.25, -2.5, -0.25, 2.5, 0.25, -2.5, 0.25 };
-    float banner2_texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
-    std::memcpy(states[0].banners[1].vertices, banner2_vertices, sizeof(states[0].banners[1].vertices));
-    std::memcpy(states[0].banners[1].texCoords, banner2_texCoords, sizeof(states[0].banners[1].texCoords));
+    
+    
+    //load banner textures
+    GLuint winBannerID = LoadTexture("win.png");
+    GLuint loseBannerID = LoadTexture("lost.png");
+    // initialize banners in all levels
+    for (int i = 0; i < LEVELS; i++) {
+        GLuint banner_textures[2] = {winBannerID, loseBannerID};
+        initBanner(states[i].banners, banner_textures);
+    }
 
 
     // set program view, model, and projection matricies
@@ -318,7 +304,7 @@ void ProcessInput() {
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
             case SDLK_SPACE:
-                states[0].player.Jump(5.0f);
+                states[currentLevel].player.Jump(5.0f);
                 break;
 
             }
@@ -327,18 +313,18 @@ void ProcessInput() {
     }
 
     // reset player velocity to prevent continuous movement
-    states[0].player.velocity.x = 0;
+    states[currentLevel].player.velocity.x = 0;
 
     // Check for pressed/held keys below
     const Uint8* keys = SDL_GetKeyboardState(NULL);
 
     if (keys[SDL_SCANCODE_A]) {
-        states[0].player.velocity.x = -3.0f;
-        states[0].player.entityDir = LEFT;
+        states[currentLevel].player.velocity.x = -3.0f;
+        states[currentLevel].player.entityDir = LEFT;
     }
     else if (keys[SDL_SCANCODE_D]) {
-        states[0].player.velocity.x = 3.0f;
-        states[0].player.entityDir = RIGHT;
+        states[currentLevel].player.velocity.x = 3.0f;
+        states[currentLevel].player.entityDir = RIGHT;
     }
 }
 
@@ -362,7 +348,7 @@ void Update() {
     while (deltaTime >= FIXED_TIMESTEP) {
 
         // check if player lost
-        if (states[0].player.entityState == DEAD) {
+        if (states[currentLevel].player.entityState == DEAD) {
             gameLost = true;
         }
         else if (!start and liveCount == 0) {
@@ -370,48 +356,48 @@ void Update() {
         }
 
         // check and update player against enemies
-        states[0].player.Update(FIXED_TIMESTEP, states[0].enemies, 3);
+        states[currentLevel].player.Update(FIXED_TIMESTEP, states[currentLevel].enemies, 3);
  
 
         // check and update player against platforms
-        states[0].player.Update(FIXED_TIMESTEP, states[0].platforms, 16);
+        states[currentLevel].player.Update(FIXED_TIMESTEP, states[currentLevel].platforms, 16);
 
-        // check and update enemies against platforms and player
-        for (int i = 0; i < 3; i++) {
+        // check and update all enemies in current level against platforms and player
+        for (int i = 0; i < enemyCount[currentLevel]; i++) {
 
             // dont update if dead
-            if (states[0].enemies[i].entityState != DEAD) {
-                states[0].enemies[i].Update(FIXED_TIMESTEP, states[0].platforms, 16);
+            if (states[currentLevel].enemies[i].entityState != DEAD) {
+                states[currentLevel].enemies[i].Update(FIXED_TIMESTEP, states[currentLevel].platforms, 16);
             }
 
             // start autonomous routine for enemies
-            states[0].enemies[i].startWalk();
-            states[0].enemies[i].startJump();
-            states[0].enemies[i].startAI(states[0].player);
+            states[currentLevel].enemies[i].startWalk();
+            states[currentLevel].enemies[i].startJump();
+            states[currentLevel].enemies[i].startAI(states[currentLevel].player);
 
-            // update enemey walking direction
-            if (states[0].enemies[i].sensorLeftCol and !states[0].enemies[i].sensorRightCol) {
-                states[0].enemies[i].entityDir = LEFT;
-                states[0].enemies[i].textureID = states[0].enemies[i].textures[0];
+            // update enemey walking direction and texture that corresponds with it
+            if (states[currentLevel].enemies[i].sensorLeftCol and !states[currentLevel].enemies[i].sensorRightCol) {
+                states[currentLevel].enemies[i].entityDir = LEFT;
+                states[currentLevel].enemies[i].textureID = states[currentLevel].enemies[i].textures[0];
             }
-            else if (!states[0].enemies[i].sensorLeftCol and states[0].enemies[i].sensorRightCol) {
-                states[0].enemies[i].entityDir = RIGHT;
-                states[0].enemies[i].textureID = states[0].enemies[i].textures[1];
+            else if (!states[currentLevel].enemies[i].sensorLeftCol and states[currentLevel].enemies[i].sensorRightCol) {
+                states[currentLevel].enemies[i].entityDir = RIGHT;
+                states[currentLevel].enemies[i].textureID = states[currentLevel].enemies[i].textures[1];
             }
-           else if (states[0].enemies[i].velocity.x > 0 and states[0].enemies[i].entityState==AI) {
-                states[0].enemies[i].textureID = states[0].enemies[i].textures[1];
+           else if (states[currentLevel].enemies[i].velocity.x > 0 and states[currentLevel].enemies[i].entityState==AI) {
+                states[currentLevel].enemies[i].textureID = states[currentLevel].enemies[i].textures[1];
             }
-            else if (states[0].enemies[i].velocity.x < 0 and states[0].enemies[i].entityState == AI) {
-                states[0].enemies[i].textureID = states[0].enemies[i].textures[0];
+            else if (states[currentLevel].enemies[i].velocity.x < 0 and states[currentLevel].enemies[i].entityState == AI) {
+                states[currentLevel].enemies[i].textureID = states[currentLevel].enemies[i].textures[0];
             }
         }
 
-        // update player walking direction
-        if (states[0].player.entityDir == LEFT) {
-            states[0].player.textureID = states[0].player.textures[0];
+        // update player walking direction texture that corresponds with it
+        if (states[currentLevel].player.entityDir == LEFT) {
+            states[currentLevel].player.textureID = states[currentLevel].player.textures[0];
         }
-        else if (states[0].player.entityDir == RIGHT) {
-            states[0].player.textureID = states[0].player.textures[1];
+        else if (states[currentLevel].player.entityDir == RIGHT) {
+            states[currentLevel].player.textureID = states[currentLevel].player.textures[1];
         }
 
         deltaTime -= FIXED_TIMESTEP;
@@ -419,12 +405,11 @@ void Update() {
 
     accumulator = deltaTime;
 
-    if (states[0].player.lifeLock) {
-
-    
-    states[0].player.lives--;
-    states[0].player.position = startPosition;
-    states[0].player.lifeLock = false;
+    // control count of player lives left
+    if (states[currentLevel].player.lifeLock) {
+    states[currentLevel].player.lives--;
+    states[currentLevel].player.position = startPosition;
+    states[currentLevel].player.lifeLock = false;
     }
 }
 
@@ -435,40 +420,38 @@ void Render() {
 
     if (gameWon) {
         //render won banner
-        states[0].banners[0].Render(&program);
+        states[currentLevel].banners[0].Render(&program);
     }
     else if (gameLost) {
         //render lost banner
-        states[0].banners[1].Render(&program);
+        states[currentLevel].banners[1].Render(&program);
     }
     else {
-        
         // render player
-        states[0].player.Render(&program);
-
-        //GENERALIZE ALL OF THIS INTO A FUNCTION
-        /////////////////////////////////////////
-        // render non-dead enemies
+        states[currentLevel].player.Render(&program);
+        
+        // render non-dead enemies in current level
         liveCount = 0;
-        for (int i = 0; i < 3; i++) {
-            if (states[0].enemies[i].entityState != DEAD) {
+        for (int i = 0; i < enemyCount[currentLevel]; i++) {
+            if (states[currentLevel].enemies[i].entityState != DEAD) {
                 start = false;
                 liveCount++;
-                states[0].enemies[i].Render(&program);
+                states[currentLevel].enemies[i].Render(&program);
             }
         }
 
-        // render platforms
-        for (int i = 0; i < 16; i++) {
-            states[0].platforms[i].Render(&program);
+        // render platforms in current level
+        for (int i = 0; i < MAX_PLAT; i++) {
+            states[currentLevel].platforms[i].Render(&program);
         }
-        /////////////////////////////////////////
+
     }
+    
     // swap to new frame
     SDL_GL_SwapWindow(displayWindow);
 
     //DEBUG
-    std::cout << states[0].player.lives << "\n";
+    std::cout << states[currentLevel].player.lives << "\n";
 }
 
 
@@ -480,7 +463,8 @@ void Shutdown() {
 // main
 int main(int argc, char* argv[]) {
     Initialize();
-
+    
+    // master game loop
     while (gameIsRunning) {
         ProcessInput();
         Update();
